8c108ceb209ed4c7d383190e27feb43fc4c29d34
Joseph <70120980+KitchenPizzeria@users.noreply.github.com>
2021-10-21 15:05:30 +0100

    Update

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -5,4 +5,5 @@ def printFromFunction() {
 def replaceString(String text) {
   text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
   writeFile file: "index.html", text: text
+  return text
 }
