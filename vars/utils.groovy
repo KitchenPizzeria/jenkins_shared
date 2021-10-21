be15adad0852bed54bcfde1187eb078510037485
Joseph <70120980+KitchenPizzeria@users.noreply.github.com>
2021-10-21 15:02:48 +0100

    UPdate

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -2,8 +2,7 @@ def printFromFunction() {
   println("I am printing from a function")
 }
 
-def replaceString() {
-  def text = readFile file: "index.html"
+def replaceString(String text) {
   text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
   writeFile file: "index.html", text: text
 }
